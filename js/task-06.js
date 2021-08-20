9b84d38b6f501684793c4a23d216874699fa9c03
Arsen Temurian <arsentim2611@gmail.com>
2021-08-20 23:10:42 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,26 @@
+// const inputRef = document.querySelector('#validation-input');
+// const getDataLength = Number(inputRef.getAttribute('data-length'));
+// const options = {
+//   inputRef: document.querySelector('#validation-input'),
+//   getDataLength: Number(inputRef.getAttribute('data-length')),
+// };
+// inputRef.addEventListener('input', () => {
+//   const { inputRef, getDataLength } = options;
+//   inputRef.classList.add('invalid');
+//   if (inputRef.value.length === getDataLength) {
+//     inputRef.classList.replace('invalid', 'valid');
+//   } else {
+//     inputRef.classList.replace('valid', 'invalid');
+//   }
+// });
+
 const inputRef = document.querySelector('#validation-input');
 const getDataLength = Number(inputRef.getAttribute('data-length'));
 const options = {
   inputRef: document.querySelector('#validation-input'),
   getDataLength: Number(inputRef.getAttribute('data-length')),
 };
-inputRef.addEventListener('input', () => {
+inputRef.addEventListener('blur', () => {
   const { inputRef, getDataLength } = options;
   inputRef.classList.add('invalid');
   if (inputRef.value.length === getDataLength) {
